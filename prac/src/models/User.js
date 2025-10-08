d3aeffa36931ff5fef587a2b5156a94dd5a973aa
Charlene Nepomuceno <charnepomuceno@gbox.adnu.edu.ph>
2025-10-08 17:21:57 +0800

    Create User model with fields: id, username, password, role.

--- prac/src/models/User.js
+++ prac/src/models/User.js
@@ -11,17 +11,14 @@ const UserSchema = new Schema(
 	{ timestamps: true }
 );
 
-// Map _id to id and remove sensitive/internal fields when converting to JSON
 UserSchema.set('toJSON', {
 	transform: (doc, ret) => {
 		ret.id = ret._id;
 		delete ret._id;
 		delete ret.__v;
-		// Do not leak password
 		delete ret.password;
 		return ret;
 	},
 }); 
 
-// Avoid model overwrite issues in watch/dev environments
 module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
