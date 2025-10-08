e950fa29b211f14c26a263eab2ddc66b9336e5cc
Nyakiraa <rabad@gbox.adnu.edu.ph>
2025-10-08 19:08:52 +0800

    TASK-6

---
+++ prac/src/app/route/Taskroute.js
@@ -0,0 +1,18 @@
+// src/app/api/tasks/route.js
+const express = require('express');
+const router = express.Router();
+const taskController = require('../../controller/taskController');
+
+// POST /api/tasks - Create a new task
+router.post('/', taskController.createTask);
+
+// GET /api/tasks - Get all tasks
+router.get('/', taskController.getTasks);
+
+// PUT /api/tasks/:id - Update a task by ID
+router.put('/:id', taskController.updateTask);
+
+// DELETE /api/tasks/:id - Delete a task by ID
+router.delete('/:id', taskController.deleteTask);
+
+module.exports = router;
