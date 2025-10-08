02938d12a03cafe055fcf2c78c3d2ff30d20b7be
Charlene Nepomuceno <charnepomuceno@gbox.adnu.edu.ph>
2025-10-08 17:48:57 +0800

    Finalized the User model with fields: id, username, password, role.

--- prac/src/models/User.js
+++ prac/src/models/User.js
@@ -5,25 +5,7 @@ const UserSchema = new mongoose.Schema(
 		username: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		role: { type: String, enum: ['user', 'admin'], default: 'user' },
-	},
-	{ timestamps: true }
+	}
 );
 
-<<<<<<< Updated upstream
-// Map _id to id and remove sensitive/internal fields when converting to JSON
-UserSchema.set('toJSON', {
-	transform: (doc, ret) => {
-		ret.id = ret._id;
-		delete ret._id;
-		delete ret.__v;
-		// Do not leak password
-		delete ret.password;
-		return ret;
-	},
-}); 
-
-// Avoid model overwrite issues in watch/dev environments
-module.exports = mongoose.models.User || mongoose.model('User', UserSchema);
-=======
 export default mongoose.model("User", UserSchema);
->>>>>>> Stashed changes
