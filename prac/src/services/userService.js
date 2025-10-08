76beedaa4a44efea2447472055cd4c57b9f22777
Charlene Nepomuceno <charnepomuceno@gbox.adnu.edu.ph>
2025-10-08 18:12:27 +0800

    Finalized the validator for registration & login input.

---
+++ prac/src/services/userService.js
@@ -0,0 +1,34 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const { validateRegistration } = require('../validators/authValidator');
+
+    //register a new user
+async function registerUser({ username, password, role = 'user' } = {}) {
+	const { valid, errors } = validateRegistration({ username, password });
+	if (!valid) {
+		const err = new Error('Validation failed');
+		err.status = 400;
+		err.errors = errors;
+		throw err;
+	}
+
+	// check existing username
+	const existing = await User.findOne({ username }).exec();
+	if (existing) {
+		const err = new Error('Username already exists');
+		err.status = 409;
+		throw err;
+	}
+
+	// hash password
+	const salt = bcrypt.genSaltSync(10);
+	const hashed = bcrypt.hashSync(password, salt);
+
+	const user = new User({ username, password: hashed, role });
+	const saved = await user.save();
+
+	// UserSchema.toJSON removes password and maps _id -> id
+	return saved.toJSON();
+}
+
+module.exports = { registerUser };
